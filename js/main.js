9e9dddb5d1b4be448fa3dcd9a566aa608ab81870
Archibracho <artur_terner@mail.ru>
2021-02-07 22:31:29 +0300

    Исправлены мелкие ошибки в функции

--- js/main.js
+++ js/main.js
@@ -8,20 +8,22 @@ const getRandomInteger = (min, max) => {
   const random = getNumberRange(min, max);
 
   if (min >= max || min < 0 || max < 0) {
-    console.log('Число меньше 0');
-    return;
+    throw new Error('Число меньше 0!');
   }
 
   return Math.floor(random);
 }
 
+getRandomInteger(1,10);
+
 const getRandomFloatNumber = (min, max, count) => {
   const random = getNumberRange(min, max);
 
   if (min >= max || min < 0 || max < 0) {
-    console.log('error');
-    return;
+    throw new Error('Число меньше 0!');
   }
 
   return random.toFixed(count);
 }
+
+getRandomFloatNumber(1,10,3);
