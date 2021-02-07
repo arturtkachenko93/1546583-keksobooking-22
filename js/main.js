c53e561062492c831487bdb339985eea53caa808
Archibracho <artur_terner@mail.ru>
2021-02-07 22:00:48 +0300

    Форматирование кода по линтеру

--- js/main.js
+++ js/main.js
@@ -1,30 +1,27 @@
 'use strict';
 
 const getNumberRange = (min, max) => {
-    return min + Math.random() * (max + 1 - min);
+  return min + Math.random() * (max + 1 - min);
 }
 
-
 const getRandomInteger = (min, max) => {
-    const random = getNumberRange(min, max);
+  const random = getNumberRange(min, max);
 
-    if (min >= max || min < 0 || max < 0) {
-        console.log('Число меньше 0');
-        return;
-    }
+  if (min >= max || min < 0 || max < 0) {
+    console.log('Число меньше 0');
+    return;
+  }
 
-    return Math.floor(random);
+  return Math.floor(random);
 }
 
 const getRandomFloatNumber = (min, max, count) => {
-    const random = getNumberRange(min, max);
+  const random = getNumberRange(min, max);
 
-    if (min >= max || min < 0 || max < 0) {
-
-      console.log('error');
-      return;
-    }
-
-    return random.toFixed(count);
+  if (min >= max || min < 0 || max < 0) {
+    console.log('error');
+    return;
   }
 
+  return random.toFixed(count);
+}
